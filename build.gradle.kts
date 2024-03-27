511fb0537e876e67ef617e82d2620ff41f4bd72e
Daniel Barquero <abarquero3@gmail.com>
2024-03-27 17:03:05 -0500

    providing google cloud engine config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.google.cloud.tools.gradle.appengine.appyaml.AppEngineAppYamlExtension
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -6,6 +8,8 @@ plugins {
     kotlin("jvm") version "1.9.23"
     id("io.ktor.plugin") version "2.3.9"
     kotlin("plugin.serialization") version "1.5.10"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.google.cloud.tools.appengine") version "2.4.2"
 }
 
 group = "com.example"
@@ -21,6 +25,17 @@ application {
 repositories {
     mavenCentral()
 }
+
+configure<AppEngineAppYamlExtension> {
+    stage {
+        setArtifact("build/libs/${project.name}-all.jar")
+    }
+    deploy {
+        version = "GCLOUD_CONFIG"
+        projectId = "GCLOUD_CONFIG"
+    }
+}
+
 
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
