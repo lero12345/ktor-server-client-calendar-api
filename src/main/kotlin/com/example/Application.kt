9107dc109a894762736188cf1c7cd1502b923a44
Daniel Barquero <abarquero3@gmail.com>
2024-04-04 22:07:10 -0500

    adding fix for base function

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -6,9 +6,7 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main() {
-    embeddedServer(
-        Netty, port = 8080, host = AppEnvConfig.host, module = Application::module
-    ).start(wait = true)
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
